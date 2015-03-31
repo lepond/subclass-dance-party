30416b51e33d3a845d4512a6a573aa478a3d4962
Lindsay Pond <lepond@gmail.com>
2015-03-30 17:03:54 -0700

    Refactored Dancer and BlinkyDancer and GifDancer classes/subclasses.

--- src/GifDancer.js
+++ src/GifDancer.js
@@ -1,16 +1,20 @@
-var BlinkyDancer = function(top, left, timeBetweenSteps){
+var GifDancer = function(top, left, timeBetweenSteps, gif){
   Dancer.call(this, top, left, timeBetweenSteps);
+  this.$node = $('<span class="gifdancer" ><img style="height:50px" src="'+gif+'"></span>');
+  this.setPosition(top, left);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 };
 
-BlinkyDancer.prototype = Object.create(Dancer.prototype);
-BlinkyDancer.prototype.constructor = BlinkyDancer;
-BlinkyDancer.prototype.step = function(){
+GifDancer.prototype = Object.create(Dancer.prototype);
+GifDancer.prototype.constructor = GifDancer;
+/*GifDancer.prototype.step = function(){
     // call the old version of step at the beginning of any call to this new version of step
     Dancer.prototype.step.call(this);
     // toggle() is a jQuery method to show/hide the <span> tag.
     // See http://api.jquery.com/category/effects/ for this and
     // other effects you can use on a jQuery-wrapped html tag.
-    this.$node.toggle();
+    //this.$node.toggle();
   };
+  */
+  
